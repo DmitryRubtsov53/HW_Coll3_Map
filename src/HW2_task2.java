112902fbd72946b106b6a2356cdc06014a768867
DmitryRubtsov53 <2578390@mail.ru>
2022-11-16 15:16:04 +0500

    Solved (HW Map): HW1 task 1 & 3 + HW2 task 2. Edit 1.

--- src/HW2_task2.java
+++ src/HW2_task2.java
@@ -1,29 +1,9 @@
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 
 public class HW2_task2 {
-    private int keyArr;
-    private String valueArr;
-    private static ArrayList<HW2_task2> task2Arr;
-//---------------------------------------------------------------------------
-    public HW2_task2(int keyArr, String valueArr) {
-        this.keyArr = keyArr;
-        this.valueArr = valueArr;
-    }
 
-    public int getKeyArr() { return keyArr; }
-    public String getValueArr() { return valueArr; }
-
-    public void setKeyArr(int keyArr) {
-        this.keyArr = keyArr;
-    }
-    public void setValueArr(String valueArr) {
-        this.valueArr = valueArr;
-    }
-//--------------------------------------------------------------------------------
     public static void main(String[] args) {
 
         System.out.println("Домашнее задание 2. Задача 2 _____________________________________________ ");
@@ -36,10 +16,6 @@ public class HW2_task2 {
         task2.put( 64321,"4 Семён"); task2.put(54021,"5 Ольга"); task2.put(54320,"6 Полина" );
         task2.put(74321,"7 Валя" );  task2.put( 28765,"8 Петр");  task2.put( 56709,"9 Антон");
         task2.put( 16789,"10 Олег");
-
-
-
-
 
         for (Map.Entry<Integer, String> pair: task2.entrySet()) {
             System.out.println(pair.getKey() + "\t" + pair.getValue());
